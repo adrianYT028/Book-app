dc81643ddaf9360b2e62707cc9b45e5f0cf280e8
Kartik Bhat <139968370+adrianYT028@users.noreply.github.com>
2025-06-27 04:24:47 +0530

    Delete book-notes-app directory

--- book-notes-app/db.js
+++
@@ -1,7 +0,0 @@
-const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: String(process.env.DB_PASS),  // Ensure it's a string
-    port: process.env.DB_PORT,
-});
